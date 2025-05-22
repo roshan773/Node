2ac9cbcddc9c8cd1a2afd41844153e5c46f99b44
Pakhre Roshan <pakhreroshan@gmail.com>
2025-05-22 12:46:50 +0530

    added maxofthree using CJS MOdule

---
+++ CJS-Module-practice/Maximum-number/index.js
@@ -0,0 +1,4 @@
+const maxOfThree = require("./maximim-number")
+
+const res = maxOfThree(10, 25, 15)
+console.log(res)
---
+++ CJS-Module-practice/Maximum-number/maximim-number.js
@@ -0,0 +1,5 @@
+function maxOfThree(a, b, c){
+    return Math.max(a,b,c);
+}
+
+module.exports = maxOfThree;
